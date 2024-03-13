ece07ef32e8633a07d6439aa93cd0c256236efb9
MOdevco <modevco05@gmail.com>
2024-03-13 17:37:17 +0500

    dfdsfs

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+module.exports = function () {
+    mongoose.connect('mongodb://127.0.0.1:27017/modevco')
+    .then(() => {
+        console.log('Mongo ishladi');
+    }).catch((err) => {
+        console.log('Mongoda hatolik bor', err);
+    })
+    
+}
+
